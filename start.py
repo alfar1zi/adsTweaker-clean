0dbd6f807f093bd962bf2e92c576b6c231d81c14
alfar1zi <f.alfarizii10@gmail.com>
2026-03-06 01:34:21 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,8 @@
 import os
 import uvicorn
 
+print("BOOT: starting, PORT=", os.getenv("PORT"))
+print("BOOT: has_key=", bool(os.getenv("DASHSCOPE_API_KEY")))
+
 port = int(os.getenv("PORT", "8080"))
 uvicorn.run("main:app", host="0.0.0.0", port=port)
